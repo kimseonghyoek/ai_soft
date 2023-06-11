57dc05dfdfb06bca7081e83242684a234bc47f10
overloper <overloper0225@gmail.com>
2023-06-11 13:58:27 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#navbar").load("/header/header.html");
+});
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+      var scroll = $(this).scrollTop();
+      var textScroll = -scroll/5;
+      var showText = scroll/170;
+      var promOp = scroll
+      
+      $('.text').css({'transform' : 'translate3d(0,'+ textScroll +'px,0)', 'opacity' : showText});
+  });
+});
